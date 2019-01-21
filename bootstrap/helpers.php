28dad4fd3317a5940b42f3f47b166a193907b739
8456 <8456@qq.com>
2019-01-21 15:38:58 +0800

    初始化分期付款模型

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,3 +3,9 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+// 默认的精度为小数点后两位
+function big_number($number, $scale = 2)
+{
+    return new \Moontoast\Math\BigNumber($number, $scale);
+}
