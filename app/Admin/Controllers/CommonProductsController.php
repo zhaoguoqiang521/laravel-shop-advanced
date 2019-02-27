f6e8873c0e6101e4c401d006bf82f035798857ea
8456 <8456@qq.com>
2019-02-27 11:08:03 +0800

    同步商品数据至 Elasticsearch

--- app/Admin/Controllers/CommonProductsController.php
+++ app/Admin/Controllers/CommonProductsController.php
@@ -9,6 +9,7 @@ use Encore\Admin\Layout\Content;
 use App\Models\Category;
 use Encore\Admin\Grid;
 use Encore\Admin\Form;
+use App\Jobs\SyncOneProductToES;
 
 abstract class CommonProductsController extends Controller
 {
@@ -100,6 +101,11 @@ abstract class CommonProductsController extends Controller
             $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
         });
 
+		$form->saved(function (Form $form) {
+            $product = $form->model();
+            $this->dispatch(new SyncOneProductToES($product));
+        });
+		
         return $form;
     }
 
